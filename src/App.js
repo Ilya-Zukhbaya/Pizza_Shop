121ee6e51c8b81995c39d04659fa3d0a6e555186
ILya-Zukhbaya <ne_perepletov@mail.ru>
2022-06-16 23:16:48 +0300

    fetching pizzaz from back and useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './scss/app.scss';
 
-import pizzas from './assets/pizzas.json';
 import { Header } from './components/Header';
 import { Categories } from './components/Categories';
 import { Sort } from './components/Sort';
 import { PizzaBlock } from './components/PizzaBlock';
 
 function App() {
+  // Сначала создаем стейт для того, чтобы получать туда данные из асинхроного бэка
+  const [items, setItems] = React.useState([]);
+  // Оборачиваем в юс эфеект, для того, чтобы не было множественных запросов из за перерисовки юз стейта, а всего один
+  React.useEffect(() => {
+    fetch('https://62ab87fba62365888bde013d.mockapi.io/items')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        // Здесь мы используем сэтАйтемс чтобы обновлять список пицц
+        setItems(data);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -22,7 +35,7 @@ function App() {
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
             {/* obj внутри метода мар, позволяет вытащить значения, которые хранятся в каждой пицце в json */}
-            {pizzas.map((obj) => (
+            {items.map((obj) => (
               <PizzaBlock
                 // Если мы уверены что названиния здешних параметров будет совпадать с теми, откуда мы их и достаем, до можно просто передать целый объект, а в пропсах вытаскивать значения (spread-оператор)
                 {...obj}
